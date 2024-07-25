3dad0af6e8f1a513f18f941109f4c8aee532e93f
XavierLopez25 <thexlopez59@gmail.com>
2024-07-25 14:55:55 -0600

    feat: added bee-hive organism rendering

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,14 @@ fn draw_block(framebuffer: &mut FrameBuffer, x: isize, y: isize) {
         framebuffer.point(x + dx, y + dy);
     }
   }
+
+  fn draw_beehive(framebuffer: &mut FrameBuffer, x: isize, y: isize) {
+    let positions = [(1, 0), (2, 0), (0, 1), (3, 1), (1, 2), (2, 2)];
+    for &(dx, dy) in &positions {
+        framebuffer.point(x + dx, y + dy);
+    }
+  }
+  
   
 fn main() {
     let window_width = 1200;
